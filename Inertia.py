eac6ac681dacf8cdbb6c365a40d05c8d05558ee4
WinstonPrice <113219264+WinstonPrice@users.noreply.github.com>
2023-05-06 12:46:06 -0700

    Create Inertia.py

---
+++ Inertia.py
@@ -0,0 +1,10 @@
+'''Program Overview: 
+        1) Ask the user what kind of shape they have
+        2) Ask the user what units they are using
+        3) Ask the user if their shape is hollow or solid
+        4) Promt the user to input their dimentions
+        5) Caclulate the second moment of inertias for them 
+        '''
+        
+#Imports 
+from math import *
